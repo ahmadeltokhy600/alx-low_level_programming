209124b8a3835c2757f22d9de5e2a126dec70ac1
ahmadeltokhy600 <8ahmed8eltokhy8@gmail.com>
2023-07-13 11:02:39 -0700

    f

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A progrsm that prints a line with puts function
+
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}		
