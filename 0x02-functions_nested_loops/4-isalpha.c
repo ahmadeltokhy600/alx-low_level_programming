7bac43cfdb9aaba10a395c2d89f885ec8be90df4
ahmadeltokhy600 <8ahmed8eltokhy8@gmail.com>
2023-07-18 08:38:32 -0700

    g

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,16 +5,12 @@
  * @c: the character to be checked
  * Return: 1 if c is a letter, 0 otherwise
  */
+
 int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
 	{
-		return(1);
-	}	
-        else
-	{
-		return(0);
+	return(1);
 	}
-	_putchar('\n');
+	return(0);
 }
-
