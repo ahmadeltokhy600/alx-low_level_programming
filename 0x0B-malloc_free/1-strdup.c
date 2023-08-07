e9cad2930d0281a5c560136ab61cf852416a2a65
ahmadeltokhy600 <8ahmed8eltokhy8@gmail.com>
2023-08-07 08:12:07 -0700

    f

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
 char *_strdup(char *str) {
     if (str == NULL) {
         return NULL;
