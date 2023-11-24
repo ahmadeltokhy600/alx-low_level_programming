a01a1fa706de477203670bb7b9fd8b66d4bc1e59
ahmadeltokhy600 <8ahmed8eltokhy8@gmail.com>
2023-11-24 02:30:31 -0800

    g

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,46 +1,23 @@
 #include "main.h"
-/**
- * binary_to_uint - converts a binary to an unsigned int
- * @b: the binary number as a string
- *
- * Return: the converted value
- */
-unsigned int binary_to_uint(const char *b)
+
+/***
+*Prototype: unsigned int binary_to_uint(const char *b)
+*
+*where b is pointing to a string of 0 and 1 chars 
+*
+*Return: the converted number, or 0 if
+*
+*there is one or more chars in the string b that is not 0 or 1 b is NULL
+*/
+unsigned int binary_to_uint(const char *b);
 {
-unsigned int decimal = 0;
-int str_len = 0, base = 1;
-
-if (!check_valid_string(b))
-return (0);
-
-while (b[str_len] != '\0')
-str_len++;
-
-while (str_len)
-{
-decimal += ((b[str_len - 1] - '0') * base);
-base *= 2;
-str_len--;
+        unsigned int number = 0;
+        if(b==0)
+        return(0);
+        for(;*b != '0' && *b != '1';*b++)
+        {
+         return(0);
+        number=number * 2 + (*b++ - '0');
+        }
+        return(number);
 }
-return (decimal);
-}
-
-/**
- * check_valid_string - checks if a string has only 0's and 1's
- * @b: string to be checked
- *
- * Return: 1 if string is valid, 0 otherwise
- */
-int check_valid_string(const char *b)
-{
-if (b == NULL)
-return (0);
-
-while (*b)
-{
-if (*b != '1' && *b != '0')
-return (0);
-b++;
-}
-return (1);
-} 
